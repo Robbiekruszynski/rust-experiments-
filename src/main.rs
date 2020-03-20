118d0be1637653e4cce6f10e78f2d4f304238a22
robbie <robbiekruszynski@gmail.com>
2020-03-20 10:21:19 -0700

    update cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use blockchainlib::*;
 
 fn main() {
-    println!("GOOD JOB, YOU IDIOT");
-    let block = Block:: new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+    println!("RUNNING");
+    let block = Block:: new(0, 0, vec![0; 32], 1, "Genesis block".to_owned());
     println!("{:?})", &block);
     let hash = block.hash();
     println!("{:?}", &hash);
+   
 }
 
