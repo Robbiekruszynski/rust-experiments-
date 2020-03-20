b99589193352794f9809c1bbe94909934fb764cd
robbie <robbiekruszynski@gmail.com>
2020-03-19 20:17:58 -0700

    compiles and prints struct of block

--- src/block.rs
+++ src/block.rs
@@ -7,28 +7,31 @@ pub struct Block {
     pub hash: BlockHash,
     pub prev_block_hash: BlockHash,
     pub nonce: u64,
+    pub payload: String,
     
 }
 
-// impl Debug for Block {
-//     fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-//         write!(f, "Block[{}: {} at: {} with {}",
-//         &self.index,
-//         &self.hash,
-//         &self.timestamp,
+impl Debug for Block {
+    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Block[{}]: {} at: {} with: {}",
+        &self.index,
+        &hex::encode(&self.hash),
+        &self.timestamp,
+        &self.payload
         
-//     )
-//     }
-// }
+    )
+    }
+}
 
 impl Block {
-    pub fn new ( index: u32, timestamp: u128, prev_block_hash: BlockHash, nonce: u64) -> Self {
+    pub fn new (index: u32, timestamp: u128, prev_block_hash: BlockHash, nonce: u64, payload: String) -> Self {
             Block {
                 index,
                 timestamp,
                 hash: vec![0; 32],
                 prev_block_hash, 
                 nonce,
+                payload,
                
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use blockchainlib::*;
+
 fn main() {
     println!("GOOD JOB, YOU IDIOT");
+    let block = Block:: new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+    println!("{:?})", &block);
+
 }
 
