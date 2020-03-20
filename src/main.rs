80407d387d25daa49306d9a5e435e1edbbf6cc7d
robbie <robbiekruszynski@gmail.com>
2020-03-19 20:58:40 -0700

    add to main.js a print for the hash through the debug line 8

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     println!("GOOD JOB, YOU IDIOT");
     let block = Block:: new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
     println!("{:?})", &block);
-
+    let hash = block.hash();
+    println!("{:?}", &hash);
 }
 
